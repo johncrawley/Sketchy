33266a38479e095c5b5db4b30124a240882cb3cb
johncrawley <johncrawley1@gmail.com>
2021-07-15 23:22:29 +0100

    Improvements to the arc brush; it is now more consistent between solid and outline styles.

--- app/src/main/java/com/jacstuff/sketchy/brushes/shapes/ArcBrush.java
+++ app/src/main/java/com/jacstuff/sketchy/brushes/shapes/ArcBrush.java
@@ -2,6 +2,7 @@ package com.jacstuff.sketchy.brushes.shapes;
 
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 
 import com.jacstuff.sketchy.brushes.BrushShape;
 import com.jacstuff.sketchy.paintview.PaintGroup;
@@ -10,6 +11,7 @@ public class ArcBrush extends AbstractBrush implements Brush {
 
 
     private int arcHeight;
+    private float quarterBrushSize;
 
     public ArcBrush(Canvas canvas, PaintGroup paintGroup){
         super(canvas, paintGroup, BrushShape.TEXT);
@@ -18,9 +20,14 @@ public class ArcBrush extends AbstractBrush implements Brush {
 
     @Override
     public void onBrushTouchDown(float x, float y, Paint paint){
-        // left, top, right, bottom
+
         boolean useCenter = paint.getStyle() != Paint.Style.STROKE;
-        canvas.drawArc( - halfBrushSize, 0,  halfBrushSize, arcHeight, 200, 140, false, paint);
+        //canvas.drawArc( -halfBrushSize, 0,  halfBrushSize, arcHeight, 200, 140, false, paint);
+        Path path = new Path();
+        path.addArc(-halfBrushSize, -quarterBrushSize, halfBrushSize, arcHeight - quarterBrushSize, 200, 140);
+
+        path.close();
+        canvas.drawPath(path, paint);
     }
 
 
@@ -29,11 +36,10 @@ public class ArcBrush extends AbstractBrush implements Brush {
         onTouchDown(x, y, paint);
     }
 
-
-
     @Override
     public void setBrushSize(int brushSize){
         super.setBrushSize(brushSize);
         arcHeight = (int)(halfBrushSize * 2.25);
+        quarterBrushSize = halfBrushSize / 2f;
     }
 }
