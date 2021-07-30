c61083fc618f8bd1fda61e3d61e9a8440b7ae104
johncrawley <johncrawley1@gmail.com>
2021-07-30 12:59:52 +0100

    Upgraded gradient button icons to higher resolution, and improved look of some style button icons.

--- app/src/main/java/com/jacstuff/sketchy/controls/settings/MenuButtonsConfigurator.java
+++ app/src/main/java/com/jacstuff/sketchy/controls/settings/MenuButtonsConfigurator.java
@@ -30,12 +30,14 @@ public class MenuButtonsConfigurator extends AbstractButtonConfigurator<Integer>
 
         buttonConfig.add(R.id.shapeButton,     R.drawable.button_shape_circle,     R.id.includeShapeControls);
         buttonConfig.add(R.id.styleButton,     R.drawable.button_style_fill,       R.id.includeStyleControls);
-        buttonConfig.add(R.id.gradientButton,  R.drawable.button_gradient_disabled,R.id.includeGradientControls);
+        buttonConfig.add(R.id.gradientButton,  R.drawable.button_gradient_off,     R.id.includeGradientControls);
         buttonConfig.add(R.id.angleButton,     angleStr,                           R.id.includeAngleControls);
+
         buttonConfig.setParentLayout(R.id.controlPanelLayoutGroup2);
         buttonConfig.add(R.id.blurButton,      R.drawable.button_blur_disabled,    R.id.includeBlurControls);
         buttonConfig.add(R.id.shadowButton,    R.drawable.button_shadow_disabled,  R.id.includeShadowControls);
-        buttonConfig.add(R.id.kaleidoButton,   "K: 1",                  R.id.includeKaleidoScopeControls);
+        buttonConfig.add(R.id.kaleidoButton,   "K: 1",                        R.id.includeKaleidoScopeControls);
+
         buttonConfig.setupClickHandler();
         buttonConfig.setupClickHandler();
         layoutIds = buttonConfig.getEntries();
--- app/src/main/java/com/jacstuff/sketchy/controls/settings/gradient/GradientButtonsConfigurator.java
+++ app/src/main/java/com/jacstuff/sketchy/controls/settings/gradient/GradientButtonsConfigurator.java
@@ -20,13 +20,13 @@ public class GradientButtonsConfigurator extends AbstractButtonConfigurator<Grad
     @Override
     public void configure(){
         buttonConfig = new ButtonConfigHandler<>(activity, this, ButtonCategory.GRADIENT, R.id.gradientOptionsLayout);
-        buttonConfig.add(R.id.noGradientButton,                 R.drawable.button_gradient_disabled,            GradientType.NONE);
-        buttonConfig.add(R.id.gradientVerticallMirrorButton,    R.drawable.gradient_vertical_mirror_button,     GradientType.VERTICAL_MIRROR);
-        buttonConfig.add(R.id.gradientHorizontalMirrorButton,   R.drawable.gradient_horizontal_mirror_button,   GradientType.HORIZONTAL_MIRROR);
-        buttonConfig.add(R.id.diagonalMirrorGradientButton, R.drawable.diagonal_mirror_gradient_button, GradientType.DIAGONAL_MIRROR);
-        buttonConfig.add(R.id.gradientRadialClampButton,    R.drawable.gradient_radial_clamp_button,    GradientType.RADIAL_CLAMP);
-        buttonConfig.add(R.id.gradientRadialRepeatButton,   R.drawable.gradient_radial_repeat_button,   GradientType.RADIAL_REPEAT);
-        buttonConfig.add(R.id.gradientRadialMirrorButton,   R.drawable.gradient_radial_mirror_button,   GradientType.RADIAL_MIRROR);
+        buttonConfig.add(R.id.noGradientButton,                 R.drawable.button_gradient_off,            GradientType.NONE);
+        buttonConfig.add(R.id.gradientVerticallMirrorButton,    R.drawable.button_gradient_vertical,     GradientType.VERTICAL_MIRROR);
+        buttonConfig.add(R.id.gradientHorizontalMirrorButton,   R.drawable.button_gradient_horizontal,   GradientType.HORIZONTAL_MIRROR);
+        buttonConfig.add(R.id.diagonalMirrorGradientButton, R.drawable.button_gradient_diagonal, GradientType.DIAGONAL_MIRROR);
+        buttonConfig.add(R.id.gradientRadialClampButton,    R.drawable.button_gradient_radial_clamp,    GradientType.RADIAL_CLAMP);
+        buttonConfig.add(R.id.gradientRadialRepeatButton,   R.drawable.button_gradient_radial_repeat,   GradientType.RADIAL_REPEAT);
+        buttonConfig.add(R.id.gradientRadialMirrorButton,   R.drawable.button_gradient_radial_mirror,   GradientType.RADIAL_MIRROR);
         buttonConfig.setupClickHandler();
         buttonConfig.setParentButton(R.id.gradientButton);
         buttonConfig.setDefaultSelection(R.id.noGradientButton);
