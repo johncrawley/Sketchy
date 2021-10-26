d58d1bdd95e4285c2dc5ce397402033529834b00
johncrawley <johncrawley1@gmail.com>
2021-10-26 15:43:18 +0100

    Fixed issue with brush size seek bar not getting enabled after using rect brush.

--- app/src/main/java/com/jacstuff/sketchy/brushes/shapes/initializer/DefaultInitializer.java
+++ app/src/main/java/com/jacstuff/sketchy/brushes/shapes/initializer/DefaultInitializer.java
@@ -9,17 +9,21 @@ import com.jacstuff.sketchy.viewmodel.MainViewModel;
 public class DefaultInitializer implements BrushInitializer {
 
     private MainViewModel viewModel;
+    private PaintHelperManager paintHelperManager;
 
 
     @Override
     public void init(Activity activity, MainViewModel viewModel, PaintHelperManager paintHelperManager){
         paintHelperManager.getBrushSizeSeekBarManager().setCurrentShapeAffectedByBrushSize(true);
         this.viewModel = viewModel;
+        this.paintHelperManager = paintHelperManager;
+
     }
 
 
     @Override
     public void initialize(){
+        paintHelperManager.getBrushSizeSeekBarManager().setCurrentShapeAffectedByBrushSize(true);
         viewModel.shadowOffsetType = ShadowOffsetType.USE_SHAPE_WIDTH;
     }
 
