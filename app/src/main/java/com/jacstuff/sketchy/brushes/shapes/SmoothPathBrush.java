75268af5606f9c2a4f200145136f6889fa8d7af8
john <johncrawley1@gmail.com>
2023-06-07 16:45:11 +0100

    Improved averaging algorithm and added a join-up for first and last points (if close) for smooth path brush.

--- app/src/main/java/com/jacstuff/sketchy/brushes/shapes/SmoothPathBrush.java
+++ app/src/main/java/com/jacstuff/sketchy/brushes/shapes/SmoothPathBrush.java
@@ -15,23 +15,34 @@ import java.util.List;
 public class SmoothPathBrush extends AbstractBrush implements Brush {
 
     private Path path, smoothPath;
-    private final List<Point> smoothPoints;
+    private List<Point> inputPoints;
+    private List<Point> smoothPoints;
     private boolean isSmoothPathReady;
+    private Paint movePaint;
+    private List<Point> tempPoints;
 
     public SmoothPathBrush(){
         super(BrushShape.PATH);
         brushInitializer = new LineInitializer();
         drawerType = DrawerFactory.Type.SMOOTH_PATH;
-        smoothPoints = new ArrayList<>(1000);
+        initPointsLists();
     }
 
 
+    private void initPointsLists(){
+        int initialCapacity = 1000;
+        inputPoints = new ArrayList<>(initialCapacity);
+        smoothPoints = new ArrayList<>(initialCapacity);
+        tempPoints  = new ArrayList<>(initialCapacity);
+    }
+
+    @Override
     public void onBrushTouchDown(Point p, Canvas canvas, Paint paint){
         setBrushSize((int)paint.getStrokeWidth());
-        path = new Path();
-        path.moveTo(p.x, p.y);
-        smoothPoints.clear();
-        smoothPoints.add(p);
+        initPath(p);
+        initMovePaint(paint);
+        init(inputPoints, p);
+        init(smoothPoints, p);
         isSmoothPathReady = false;
     }
 
@@ -40,15 +51,38 @@ public class SmoothPathBrush extends AbstractBrush implements Brush {
     public void onTouchMove(Point p, Canvas canvas, Paint paint){
         currentStyle.onDraw();
         path.lineTo(p.x, p.y);
-        smoothPoints.add(p);
-        canvas.drawPath(path, paint);
+        inputPoints.add(p);
+        canvas.drawPath(path, movePaint);
     }
+
 
 
     @Override
     public void onTouchUp(Point p, Canvas canvas, Paint paint){
-       setupSmoothPath(p);
-       canvas.drawPath(smoothPath, paint);
+        if(inputPoints.size() < 5){
+            canvas.drawPath(path, movePaint);
+            return;
+        }
+        setupSmoothPath(p);
+        canvas.drawPath(smoothPath, paint);
+    }
+
+
+    private void initPath(Point p){
+        path = new Path();
+        path.moveTo(p.x, p.y);
+    }
+
+
+    private void initMovePaint(Paint paint){
+        movePaint = new Paint(paint);
+        movePaint.setStyle(Paint.Style.STROKE);
+    }
+
+
+    private void init(List<Point> pointsList, Point firstPoint){
+        pointsList.clear();
+        pointsList.add(firstPoint);
     }
 
 
@@ -63,24 +97,81 @@ public class SmoothPathBrush extends AbstractBrush implements Brush {
 
 
     private void calculateSmoothPoints(){
-        int numberOfRounds = 3;
-        for(int i=0; i<numberOfRounds; i++){
+        for(int i = 0, numberOfRounds = 3; i < numberOfRounds; i++){
             adjustSmoothPoints();
+        }
+        joinFirstAndLastPointsIfCloseTogether();
+    }
+
+
+    private void joinFirstAndLastPointsIfCloseTogether(){
+        int proximityThreshold = 40;
+        Point firstPoint = smoothPoints.get(0);
+        Point lastPoint = smoothPoints.get(smoothPoints.size()-1);
+        if(arePointsClose(firstPoint, lastPoint, proximityThreshold)){
+            adjustPointsToAverage(firstPoint, lastPoint);
         }
     }
 
 
+    private boolean arePointsClose(Point p1, Point p2, int minDistance){
+        return areCoordinatesClose(p1.x, p2.x, minDistance)
+                && areCoordinatesClose(p1.y, p2.y, minDistance);
+    }
+
+
+    private void adjustPointsToAverage(Point p1, Point p2){
+        int avgX = (p1.x + p2.x) /2;
+        int avgY = (p1.y + p2.y) /2;
+        p1.x = avgX;
+        p1.y = avgY;
+        p2.x = avgX;
+        p2.y = avgY;
+    }
+
+
+    private boolean areCoordinatesClose(int a1, int a2, int minDistance){
+        return Math.abs(a1 - a2) < minDistance;
+    }
+
+
     private void adjustSmoothPoints(){
-        if(smoothPoints.size() < 5){
-            return;
+        clearListAndFirstPointTo(smoothPoints);
+        clearListAndFirstPointTo(tempPoints);
+        for(int i = 1; i < inputPoints.size()-1; i++){
+            Point p = new Point(getAveragePointX(i), getAveragePointY(i));
+            smoothPoints.add(p);
+            tempPoints.add(p);
         }
-        for(int i = 1; i < smoothPoints.size()-1; i++){
-            Point point = smoothPoints.get(i);
-            int x = (point.x + smoothPoints.get(i-1).x + smoothPoints.get(i+1).x)/3;
-            int y = (point.y + smoothPoints.get(i-1).y + smoothPoints.get(i+1).y)/3;
-            point.x = x;
-            point.y = y;
-        }
+        addLastPointTo(smoothPoints);
+        addLastPointTo(tempPoints);
+        inputPoints.clear();
+        inputPoints.addAll(tempPoints);
+    }
+
+
+    private void clearListAndFirstPointTo(List<Point> pointList){
+        pointList.clear();
+        pointList.add(inputPoints.get(0));
+    }
+
+
+    private void addLastPointTo(List<Point> pointList){
+        pointList.add(inputPoints.get(inputPoints.size()-1));
+    }
+
+
+    private int getAveragePointX(int index){
+        return (inputPoints.get(index).x
+                + inputPoints.get(index-1).x
+                + inputPoints.get(index+1).x)/3;
+    }
+
+
+    private int getAveragePointY(int index){
+        return (inputPoints.get(index).y
+                + inputPoints.get(index-1).y
+                + inputPoints.get(index+1).y)/3;
     }
 
 
@@ -88,12 +179,12 @@ public class SmoothPathBrush extends AbstractBrush implements Brush {
         smoothPath = new Path();
         smoothPath.moveTo(smoothPoints.get(0).x - negativeOffset.x,
                 smoothPoints.get(0).y - negativeOffset.y);
+
         for(int i=1; i<smoothPoints.size(); i++){
             smoothPath.lineTo(smoothPoints.get(i).x - negativeOffset.x,
                     smoothPoints.get(i).y - negativeOffset.y);
         }
     }
-
 
 
 }
