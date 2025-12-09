af7f47e99dc456e557c5dd6c33ec76ed997b4d71
john <johncrawley1@gmail.com>
2025-12-09 23:59:11 +0000

    Added a background resource for improved icon.

--- app/src/main/java/com/jacstuff/sketchy/brushes/shapes/drawer/ShapeDrawer.java
+++ app/src/main/java/com/jacstuff/sketchy/brushes/shapes/drawer/ShapeDrawer.java
@@ -1,13 +1,14 @@
 package com.jacstuff.sketchy.brushes.shapes.drawer;
 
 import android.graphics.Paint;
+import android.graphics.PointF;
 
 import com.jacstuff.sketchy.brushes.shapes.Brushable;
 
 public interface ShapeDrawer {
 
-    void down(float x, float y, Paint paint);
-    void move(float x, float y, Paint paint);
-    void up(float x, float y, Paint paint);
+    void down(PointF point, Paint paint);
+    void move(PointF point, Paint paint);
+    void up(PointF point, Paint paint);
     void setBrushShape(Brushable brushShape);
 }
---
+++ app/src/main/java/com/jacstuff/sketchy/brushes/shapes/drawer/SimpleShapeDrawer.java
@@ -0,0 +1,30 @@
+package com.jacstuff.sketchy.brushes.shapes.drawer;
+
+import android.graphics.Paint;
+import android.graphics.PointF;
+
+import com.jacstuff.sketchy.brushes.shapes.Brushable;
+
+public class SimpleShapeDrawer implements ShapeDrawer{
+
+
+    @Override
+    public void down(PointF point, Paint paint) {
+
+    }
+
+    @Override
+    public void move(PointF point, Paint paint) {
+
+    }
+
+    @Override
+    public void up(PointF point, Paint paint) {
+
+    }
+
+    @Override
+    public void setBrushShape(Brushable brushShape) {
+
+    }
+}
