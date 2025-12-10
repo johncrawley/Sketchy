e30a9319528280b134b5aae86b681eac777dfefd
john <johncrawley1@gmail.com>
2025-12-10 23:59:30 +0000

    Created AbstractShapeDrawer and building out SimpleShapeDrawer.

---
+++ app/src/main/java/com/jacstuff/sketchy/brushes/shapes/drawer/AbstractShapeDrawer.java
@@ -0,0 +1,121 @@
+package com.jacstuff.sketchy.brushes.shapes.drawer;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
+
+import com.jacstuff.sketchy.brushes.shapes.Brush;
+import com.jacstuff.sketchy.brushes.shapes.Brushable;
+import com.jacstuff.sketchy.paintview.PaintView;
+import com.jacstuff.sketchy.paintview.helpers.KaleidoscopeHelper;
+import com.jacstuff.sketchy.paintview.helpers.PaintHelperManager;
+import com.jacstuff.sketchy.paintview.helpers.TileHelper;
+import com.jacstuff.sketchy.paintview.helpers.placement.PlacementHelper;
+
+public class AbstractShapeDrawer implements ShapeDrawer {
+
+    final PaintHelperManager paintHelperManager;
+    final KaleidoscopeHelper kaleidoscopeHelper;
+    final KaleidoscopeDrawer kaleidoscopeDrawer;
+    final TileHelper tileHelper;
+    final Canvas canvas;
+    Brush brush;
+    PaintView paintView;
+    boolean isColorChangedOnDown = true;
+    final Paint shadowPaint;
+    final Paint paint;
+    final PlacementHelper placementHelper;
+    protected boolean isDrawOnMoveModeEnabled;
+    Brushable brushShape;
+
+
+    AbstractShapeDrawer(PaintView paintView){
+        this.canvas =  paintView.getCanvas();
+        this.paintHelperManager = paintView.getPaintHelperManager();
+        kaleidoscopeHelper = paintHelperManager.getKaleidoscopeHelper();
+        placementHelper = paintHelperManager.getPlacementHelper();
+        tileHelper = paintHelperManager.getTileHelper();
+        kaleidoscopeDrawer = new KaleidoscopeDrawer(paintView, kaleidoscopeHelper);
+        shadowPaint = paintView.getShadowPaint();
+        paint = paintView.getPaint();
+    }
+
+
+    public void setBrushShape(Brushable brushShape){
+        this.brushShape = brushShape;
+    }
+
+
+    public boolean isDrawOnMoveModeEnabled(){
+        return isDrawOnMoveModeEnabled;
+    }
+
+
+    public void setIsDrawOnMoveModeEnabled(boolean isEnabled){
+        this.isDrawOnMoveModeEnabled = isEnabled;
+    }
+
+
+    public void setBrush(Brush brush){
+        this.brush = brush;
+    }
+
+
+    public void init(){
+
+        //kaleidoscopeDrawer.initParentDrawer(this);
+    }
+
+
+    public void setPaintView(PaintView paintView){
+        this.paintView = paintView;
+    }
+
+
+    void updateColorGradientAndAngle(float x, float y){
+        var isPhmNull = paintHelperManager.getColorHelper() == null;
+        log("entered updateColorGradientAndAngle() is color helper null: " + isPhmNull);
+        paintHelperManager.getColorHelper().assignColors();
+        //  updateGradientMidpoint(x,y);
+        // paintHelperManager.getAngleHelper().updateAngle();
+    }
+
+
+    private void log(String msg){
+        System.out.println("^^^Abstract Drawer: " + msg);
+    }
+
+
+    void updateGradientMidpoint(float x, float y){
+        paintHelperManager.getGradientHelper().assignGradient(x, y);
+    }
+
+
+    public void initExtra(){
+        //do nothing
+    }
+
+
+    public boolean isColorChangedOnDown(){
+        return isColorChangedOnDown;
+    }
+
+
+    @Override
+    public void down(PointF point, Paint paint) {
+        //do nothing
+    }
+
+
+    @Override
+    public void move(PointF point, Paint paint) {
+        //do nothing
+    }
+
+
+    @Override
+    public void up(PointF point, Paint paint) {
+        //do nothing
+    }
+
+}
--- app/src/main/java/com/jacstuff/sketchy/brushes/shapes/drawer/SimpleShapeDrawer.java
+++ app/src/main/java/com/jacstuff/sketchy/brushes/shapes/drawer/SimpleShapeDrawer.java
@@ -1,30 +1,66 @@
 package com.jacstuff.sketchy.brushes.shapes.drawer;
 
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.PointF;
 
 import com.jacstuff.sketchy.brushes.shapes.Brushable;
 
-public class SimpleShapeDrawer implements ShapeDrawer{
+public class SimpleShapeDrawer extends AbstractShapeDrawer{
 
 
     @Override
     public void down(PointF point, Paint paint) {
-
+        drawToCanvas(x,y, paint);
     }
+
 
     @Override
     public void move(PointF point, Paint paint) {
 
     }
 
+
     @Override
     public void up(PointF point, Paint paint) {
 
     }
 
+
     @Override
     public void setBrushShape(Brushable brushShape) {
 
+    }
+
+
+    void drawToCanvas(float x, float y, Paint paint){
+        if(kaleidoscopeHelper.isEnabled()){
+            kaleidoscopeDrawer.drawKaleidoscope(x,y, paint);
+        }
+        else if(paintHelperManager.getTileHelper().isEnabled()){
+            paintHelperManager.getTileHelper().draw(x,y, this);
+        }
+        else{
+            rotateAndDraw(x,y, paint);
+        }
+        paintView.invalidate();
+    }
+
+
+    public void rotateAndDraw(float x, float y, Paint paint){
+        canvas.save();
+        canvas.translate(x, y);
+        Point p = new Point((int)x, (int)y);
+        canvas.rotate(paintHelperManager.getAngleHelper().getFineAngle());
+        if(paintHelperManager.getShadowHelper().isShadowEnabled()){
+            brush.onTouchDown(p, canvas, paintView.getShadowPaint());
+        }
+        brush.onTouchDown(p, canvas, paint);
+        canvas.restore();
+    }
+
+
+    public void drawKaleidoscopeSegment(float x, float y, Paint paint){
+        rotateAndDraw(x - kaleidoscopeHelper.getCenterX(), y - kaleidoscopeHelper.getCenterY(), paint);
     }
 }
